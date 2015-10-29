94b199b5d22cc3ec8f8dd66af4ba51abdad0f518
Carlos Matias <carlos.matias93@gmail.com>
2015-10-29 15:54:45 +0000

    Changed Db connection.

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     'secret': 'cmovtrainticketsystemsecretkey',
-    'database' :'mongodb://localhost:27017/test'
-    //'database': 'mongodb://admin:password@ds033744.mongolab.com:33744/cmov-p1'
+    //'database' :'mongodb://localhost:27017/test'
+    'database': 'mongodb://admin:password@ds033744.mongolab.com:33744/cmov-p1'
 };
