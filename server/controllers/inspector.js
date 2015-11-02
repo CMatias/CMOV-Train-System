df1235ff1a840fd9e2cb156d2639b75a3deae10b
Carlos Matias <carlos.matias93@gmail.com>
2015-11-02 18:43:09 +0000

    Added duration to get inspector trips.

--- server/controllers/inspector.js
+++ server/controllers/inspector.js
@@ -16,11 +16,27 @@ exports.getTrips = function(req, res) {
     Inspector
         .findById(req.decoded._id)
         .populate("_trips")
-        .exec(function (err, inspectors) {
+        .exec(function (err, inspector) {
             if (err) {
                 res.send(err);
             }
-            res.json(inspectors._trips);
+            var trips = inspector._trips;
+            var ret = [];
+            for(var i = 0; i < trips.length; i++){
+                var timeDurDate = new Date(new Date(trips[i].stops[trips[i].stops.length-1].date) - new Date(trips[i].stops[0].date));
+                var timeDurObj = {
+                    "tripDuration": {
+                        "hours": timeDurDate.getHours(),
+                        "minutes": timeDurDate.getMinutes(),
+                        "seconds": timeDurDate.getSeconds()
+                    }
+                };
+                var retObj = [];
+                retObj.push(trips[i]);
+                retObj.push(timeDurObj);
+                ret.push(retObj);
+            }
+            res.send(ret);
         });
 };
 
